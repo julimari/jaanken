4def7f8d7c155c87a2f5437d79b4c22f8f045051
IWATA JULIANA <44888050+julimari@users.noreply.github.com>
2021-01-10 17:38:40 +0900

    add new file

---
+++ Test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "サーバーからのお返事です！")
+
+	//呼び出される際に『w』と『r』とうい名前のデータを渡す
+	//wはユーザーに文字列の送信
+	//rはユーザーに関する情報が入っている。どのURLか、IPアドレスはなど
+}
+
+func main() {
+	muxHTTP := http.NewServeMux()
+	//httpパッケージのNewServerMuxを呼び出して、返ってきたデータをmuxHTTPと言う変数に格納
+	muxHTTP.HandleFunc("/", handler)
+	//serveMuxはhandleFuncと言う関数を使うことで『/』と言うアドレスにアクセスがあったらhandlerを呼ぶように設定している
+
+	ServerHTTP := &http.Server{
+		Addr:           ":8080",
+		Handler:        muxHTTP,
+		ReadTimeout:    10 * time.Second, //１０秒でアクセス
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20, //データの最大数
+	}
+
+	err := ServerHTTP.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
